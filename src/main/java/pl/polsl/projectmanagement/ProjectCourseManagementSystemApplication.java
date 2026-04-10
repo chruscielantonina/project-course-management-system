155ccebf87f2f44982dfde989b41811cce607dd3
chruscielantonina <chruscielnina@gmail.com>
2026-04-10 15:56:23 +0200

    Initial project setup - Spring Boot + Docker + PostgreSQL

---
+++ src/main/java/pl/polsl/projectmanagement/ProjectCourseManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.polsl.projectmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCourseManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCourseManagementSystemApplication.class, args);
+    }
+
+}
