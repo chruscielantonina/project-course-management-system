a92773408ae2bad7ddb1a510a00ac64319c1b784
JanSta02 <stanekjan02@gmail.com>
2026-04-16 19:35:28 +0200

    Add signUpForSection in StudentService

--- src/main/java/pl/polsl/projectmanagement/service/StudentService.java
+++ src/main/java/pl/polsl/projectmanagement/service/StudentService.java
@@ -3,9 +3,15 @@ package pl.polsl.projectmanagement.service;
 import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.polsl.projectmanagement.model.Section;
 import pl.polsl.projectmanagement.model.Student;
+import pl.polsl.projectmanagement.model.StudentSection;
+import pl.polsl.projectmanagement.repository.AttendanceRepository;
+import pl.polsl.projectmanagement.repository.SectionRepository;
 import pl.polsl.projectmanagement.repository.StudentRepository;
+import pl.polsl.projectmanagement.repository.StudentSectionRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -14,7 +20,31 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class StudentService {
     private final StudentRepository studentRepository;
+    private final SectionRepository sectionRepository;
+    private final StudentSectionRepository studentSectionRepository;
+    private final AttendanceRepository attendanceRepository;
 
+    public boolean signUpForSection(UUID studentID, UUID sectionID) {
+        try{
 
+            Student student = studentRepository.findById(studentID).orElseThrow();
+            Section section = sectionRepository.findById(sectionID).orElseThrow();
 
+            if (studentSectionRepository.findEnrollment(studentID, sectionID).isPresent()) {
+                return false;
+            }
+
+            StudentSection enrollment = new StudentSection();
+            enrollment.setStudent(student);
+            enrollment.setSection(section);
+            enrollment.setEnrollmentDate(LocalDate.now());
+            enrollment.setStatus("ACTIVE");
+            studentSectionRepository.save(enrollment);
+
+            return true;
+
+        } catch(Exception e) {
+            return false;
+        }
+    }
 }
