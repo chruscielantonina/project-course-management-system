0287e17b061c1993dd14a5af1c0ee7b2ebdcb3f1
JanSta02 <stanekjan02@gmail.com>
2026-04-16 19:33:41 +0200

    Add two Query based functions findEnrollment findAllbyStudentId.

--- src/main/java/pl/polsl/projectmanagement/repository/StudentSectionRepository.java
+++ src/main/java/pl/polsl/projectmanagement/repository/StudentSectionRepository.java
@@ -1,11 +1,20 @@
 package pl.polsl.projectmanagement.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.polsl.projectmanagement.model.StudentSection;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface StudentSectionRepository extends JpaRepository<StudentSection, UUID> {
     List<StudentSection> findByStudentSID(UUID studentId);
     List<StudentSection> findBySectionSeID(UUID sectionId);
+
+    @Query("SELECT ss FROM StudentSection ss WHERE ss.student.sID = :sId AND ss.section.seID = :seId")
+    Optional<StudentSection> findEnrollment(@Param("sId") UUID sId, @Param("seId") UUID seId);
+
+    @Query("SELECT ss FROM StudentSection ss WHERE ss.student.sID = :sId")
+    List<StudentSection> findAllByStudentId(@Param("sId") UUID sId);
 }
