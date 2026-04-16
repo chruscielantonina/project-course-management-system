371469e45d0871a2edc870795479fd97526b6500
JanSta02 <stanekjan02@gmail.com>
2026-04-16 20:12:44 +0200

    Add Query based functions findAllByStudentIdAndSectionId findAllByStudentId in AttendanceRepository

--- src/main/java/pl/polsl/projectmanagement/repository/AttendanceRepository.java
+++ src/main/java/pl/polsl/projectmanagement/repository/AttendanceRepository.java
@@ -1,9 +1,18 @@
 package pl.polsl.projectmanagement.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.polsl.projectmanagement.model.Attendance;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AttendanceRepository extends JpaRepository<Attendance, UUID> {
+
+    @Query("SELECT a FROM Attendance a WHERE a.student.sID = :studentId")
+    List<Attendance> findAllByStudentId(@Param("studentId") UUID studentId);
+
+    @Query("SELECT a FROM Attendance a WHERE a.student.sID = :studentId AND a.section.seID = :sectionId")
+    List<Attendance> findAllByStudentIdAndSectionId(@Param("studentId") UUID studentId, @Param("sectionId") UUID sectionId);
 }
