aa3890d00debb46985119ca98e3af654eb67bd0b
JanSta02 <stanekjan02@gmail.com>
2026-04-17 16:42:30 +0200

    Add signUpForSection in StudentController

--- src/main/java/pl/polsl/projectmanagement/controller/StudentController.java
+++ src/main/java/pl/polsl/projectmanagement/controller/StudentController.java
@@ -3,18 +3,27 @@ package pl.polsl.projectmanagement.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import pl.polsl.projectmanagement.model.Student;
+import pl.polsl.projectmanagement.dto.ChangeSectionRequest;
+import pl.polsl.projectmanagement.model.Attendance;
 import pl.polsl.projectmanagement.service.StudentService;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
-
 @RestController
 @RequestMapping("/api/students")
 @RequiredArgsConstructor
 public class StudentController {
     private final StudentService studentService;
 
+    @PostMapping("/{studentId}/sections/{sectionId}")
+    public ResponseEntity<String> signUpForSection(@PathVariable UUID studentId, @PathVariable UUID sectionId) {
+        boolean success = studentService.signUpForSection(studentId, sectionId);
+
+        if (success) {
+            return ResponseEntity.ok("Successfully signed up for the section.");
+        } else {
+            return ResponseEntity.badRequest().body("Error: Could not sign up for the section.");
+        }
+    }
 
 }
