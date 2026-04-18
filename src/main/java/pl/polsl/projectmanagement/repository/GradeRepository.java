2b4b8bb0b0d0e6d7b9ccd571f2bdd3fc6f4442a8
JanSta02 <stanekjan02@gmail.com>
2026-04-18 16:23:26 +0200

    Add GradeRepository to repositories

---
+++ src/main/java/pl/polsl/projectmanagement/repository/GradeRepository.java
@@ -0,0 +1,23 @@
+package pl.polsl.projectmanagement.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import pl.polsl.projectmanagement.model.Grade;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface GradeRepository {
+    @Query("SELECT g FROM Grade g WHERE g.studentSection.student.sID = :studentID")
+    List<Grade> findAllByStudentId(@Param("studentId")UUID studentId);
+
+    @Query("SELECT g FROM Grade g WHERE g.studentSection.student.sID = :studentId " +
+            "AND g.studentSection.section.seID = :sectionId")
+    List<Grade> findAllByStudentIdAndSectionId(@Param("studentId") UUID studentId,
+                                               @Param("sectionId") UUID sectionId);
+
+    @Query("SELECT g FROM Grade g WHERE g.studentSection.id = :studentSectionID")
+    List<Grade> findAllByStudentSectionId(@Param("studentSectionId") UUID studentSection);
+
+}
