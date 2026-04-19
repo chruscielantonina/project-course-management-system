bdd48e2fc3e439c57c5631e8c1b3333e6feac4cd
m-depta <depta.michal@outlook.com>
2026-04-19 12:56:50 +0200

    feat: add findAllWithDetails to SectionRepository

--- src/main/java/pl/polsl/projectmanagement/repository/SectionRepository.java
+++ src/main/java/pl/polsl/projectmanagement/repository/SectionRepository.java
@@ -1,6 +1,7 @@
 package pl.polsl.projectmanagement.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.polsl.projectmanagement.model.Section;
 import pl.polsl.projectmanagement.model.SectionStatus;
 import java.util.List;
@@ -8,4 +9,10 @@ import java.util.UUID;
 
 public interface SectionRepository extends JpaRepository<Section, UUID> {
     List<Section> findBySeState(SectionStatus status);
+
+    @Query("SELECT DISTINCT s FROM Section s " +
+            " JOIN FETCH s.teacher " +
+            "LEFT JOIN FETCH s.enrolledStudents ss " +
+            "LEFT JOIN FETCH ss.student")
+    List<Section> findAllWithDetails();
 }
