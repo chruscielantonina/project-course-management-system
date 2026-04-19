63281268fb7606574259fd053208857f98c8df1c
m-depta <depta.michal@outlook.com>
2026-04-19 12:31:13 +0200

    feat: add enrolled students relationship to Section

--- src/main/java/pl/polsl/projectmanagement/model/Section.java
+++ src/main/java/pl/polsl/projectmanagement/model/Section.java
@@ -2,6 +2,9 @@ package pl.polsl.projectmanagement.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -23,6 +26,9 @@ public class Section {
     @Column(nullable = false)
     private int maxCapacity;
 
+    @OneToMany(mappedBy = "section", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<StudentSection> enrolledStudents = new ArrayList<>();
+    
     @ManyToOne
     @JoinColumn(name = "topic_id", nullable = false)
     private Topic topic;
