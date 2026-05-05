9ed6718b1a3a0e4f4f2b4af0ecb1976e020868fc
m-depta <depta.michal@outlook.com>
2026-05-05 08:46:14 +0200

    feat: add AppUser model

---
+++ src/main/java/pl/polsl/projectmanagement/model/AppUser.java
@@ -0,0 +1,30 @@
+package pl.polsl.projectmanagement.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "app_users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AccountType accountType;
+}
